f9e2c7fb6a4df99442c4c65a475e2138c2f3f7cf
Matthew Goo <mattgoo@gmail.com>
2013-07-23 17:25:51 -0600

    Problems with adding beginning '[' to string

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,42 +3,54 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-	if(typeof obj === "number") {
-		return obj + "";	
-	}
-	else if(typeof obj === "string") {
-		return "\"" + obj + "\"";
-	}
-	else if(obj === null) {
-		return "null";
-	}
-	else if(obj === true) {
-		return "true";
-	}
-	else if(obj === false) {
-		return "false";
-	}
-	else if(Array.isArray(obj)) {
-		var result = [];
-		if(obj.length > 1) {
-			var workObj = obj.slice(0);
-			var len = obj.length - 1;
-			console.log(workObj);
-			result.push( stringifyJSON( workObj[0]) );
+	var inString = function (inObj, multiVal) {
+		if(typeof inObj === "number") {
+			return inObj + "";	
+		}
+		else if(typeof inObj === "string") {
+			return "\"" + inObj + "\"";
+		}
+		else if(inObj === null) {
+			return "null";
+		}
+		else if(inObj === true) {
+			return "true";
+		}
+		else if(inObj === false) {
+			return "false";
+		}
+		else if(Array.isArray(inObj)) {
+			if(inObj.length === 0) {
+				return "[]";
+			}
+			else if(typeof inObj[0] === "number") {
+				return multiVal ? inObj[0] : "[" + inObj[0] + "]";
+			}
+			else if(typeof inObj[0] === "string") {
+				return multiVal ?  "\"" + inObj[0] + "\"" : "[\"" + inObj[0] + "\"]";
+			}
+		}
+	};
 
-			workObj.shift();
+	if(Array.isArray(obj) && obj.length > 1) {
 
+		var workObj = obj.slice(0);
+		//console.log(workObj);
+		var temp;
+		temp = inString(workObj[0], true);
+		
+
+		workObj.shift();
+		//console.log(workObj);
+
+		if(workObj.length === 1) {
+			return temp + "," + inString(workObj, true) + "]";
 		}
-		if(obj.length === 0) {
-			return "[]";
+		else {
+			return temp + "," + stringifyJSON(workObj);
 		}
-		else if(typeof obj[0] === "number") {
-			result.push(obj[0]);
-		}
-		else if(typeof obj[0] === "string") {
-			result.push( "\"" + obj[0] + "\"" );
-		}
-		return "[" + result + "]";
 	}
-
+	else {
+		return inString(obj, false);
+	}
 };
