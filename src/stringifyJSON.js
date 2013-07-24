7af636c24be1f7d038becd2e42fbb240561e605c
Matthew Goo <mattgoo@gmail.com>
2013-07-23 22:30:31 -0600

    stringify completed most of objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -8,6 +8,15 @@ var stringifyJSON = function (obj, initialize) {
 		isSet = true;
 	}
 
+	var arrayDepth = function(arr) {
+		var count = 0;
+		while(Array.isArray(arr)) {
+			arr = arr[0];
+			count ++;
+		}
+		return count;
+	};
+
 	var inString = function (inObj, multiVal) {
 		if(typeof inObj === "number") {
 			return inObj + "";	
@@ -25,7 +34,24 @@ var stringifyJSON = function (obj, initialize) {
 			return "false";
 		}
 		else if(Array.isArray(inObj)) {
-			if(inObj.length === 0) {
+			if(Array.isArray(inObj[0])) {
+				var depth = arrayDepth(inObj[0]);
+				if(depth > 2) {
+					var paren = inObj[0];
+					for(var i=0; i<depth; i++) {
+						paren = paren[0];
+					}
+					paren = inString(paren);
+					for(var i=0; i<depth+1; i++) {
+						paren = "[" + paren + "]";
+					}
+					return paren;
+				}
+				else {
+					return stringifyJSON(inObj[0]);
+				}
+			}
+			else if(inObj.length === 0) {
 				return "[]";
 			}
 			else if(typeof inObj[0] === "number") {
@@ -35,21 +61,32 @@ var stringifyJSON = function (obj, initialize) {
 				return multiVal ?  "\"" + inObj[0] + "\"" : "[\"" + inObj[0] + "\"]";
 			}
 		}
+		else {
+			//console.log(obj);
+			var keys = Object.keys(obj);
+			if(keys.length === 0) {
+				return "{}";
+			}
+			else {
+				var str = "{";
+				for(var key in keys) {
+					str += stringifyJSON(keys[key]) + ":" + stringifyJSON(obj[keys[key]]) + ",";
+				}
+				str = str.substring(0,str.length-1) + "}";	// substring removes last ','
+				console.log(str);
+				return str;
+			}
+		}
 	};
 
 	if(Array.isArray(obj) && obj.length > 1) {
-
-		var workObj = obj.slice(0);
-
+		var workObj = obj.slice(0); 	// fix for overwriting original obj
 		var	temp = inString(workObj[0], true);
 		if(isSet) {
 			temp = "[" + temp;
-		} 
-
+		}
 		workObj.shift();
-
 		if(workObj.length === 1) {
-			console.log(inString(workObj,true));
 			return temp + "," + inString(workObj, true) + "]";
 		}
 		else {
