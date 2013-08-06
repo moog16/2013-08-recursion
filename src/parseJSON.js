dcc9f655f8937d9d40034424bdee96bfc0720a1d
Matthew Goo <mattgoo@gmail.com>
2013-08-05 23:30:58 -0700

    met spec up until false

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,24 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
-  // your code goes here
+	var len = json.length;
+	var num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+	var type = function(el) {
+	  if(_.contains(num, el)) {
+	  	return 'number';
+	  }
+	};
+
+	if(type(json) === 'number') {
+		return parseInt(json);
+	} else if(json === 'null') {
+		return null;
+	} else if(json === 'true') {
+		return true;
+	} else if(json === 'false') {
+		return false;
+	} else if(json[0] === '[' && json[len-1] === ']') {
+		return [json[1]];
+	}
 };
